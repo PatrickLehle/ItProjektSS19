7cb9d163b1b4dd1d5fc678f511138e7010b07d03
Patrick <anyeong@hotmail.co.kr>
2019-05-09 09:26:54 +0200

    initial commit of scart v2

---
+++ ItProjektSS19/src/de/hdm/itprojektss19/team03/scart/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package de.hdm.itprojektss19.team03.scart.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
