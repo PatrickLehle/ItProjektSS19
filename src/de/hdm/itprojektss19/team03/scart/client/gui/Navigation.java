37626f7a506a7e4b000647dd1addc910fe77c92e
Patrick <anyeong@hotmail.co.kr>
2019-05-09 13:06:23 +0200

    added BusinessObjects and Navigation for GUI

---
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/Navigation.java
@@ -0,0 +1,15 @@
+package de.hdm.itprojektss19.team03.scart.client.gui;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.cellview.client.CellTree;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class Navigation {
+
+}
