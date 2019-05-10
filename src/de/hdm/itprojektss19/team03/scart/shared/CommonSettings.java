908a2a74d036cefcb6790c2b5b7c4d57c4436e05
Patrick <anyeong@hotmail.co.kr>
2019-05-10 12:21:55 +0200

    ClientsideSettings and CommonSettings added

---
+++ src/de/hdm/itprojektss19/team03/scart/shared/CommonSettings.java
@@ -0,0 +1,12 @@
+package de.hdm.itprojektss19.team03.scart.shared;
+
+/**
+ * Zentrale Konstante etc., die sowohl Server- als auch Client-seitig benoetigt
+ * werden. Superklasse von <code>ClientsideSettings</code> und
+ * <code>ServersideSettings</code>.
+ * 
+ * @author Thies
+ */
+public class CommonSettings {
+
+}
