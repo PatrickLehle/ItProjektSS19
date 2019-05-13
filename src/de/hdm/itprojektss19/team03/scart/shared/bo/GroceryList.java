30a0c441cb38a7a222e100b894a05008988e938c
bastiantilk <bt028@hdm-stuttgart.de>
2019-05-13 09:24:11 +0200

    Attrtibutes and empty methods GroceryList class in shared.bo

---
+++ src/de/hdm/itprojektss19/team03/scart/shared/bo/GroceryList.java
@@ -0,0 +1,30 @@
+package de.hdm.itprojektss19.team03.scart.shared.bo;
+import java.util.ArrayList;
+
+public class GroceryList {
+	private long serialVersionUID;
+	private long groceryListID;
+	private Group group;
+	private String name;
+	private Arraylist<Article> articles;
+	
+	public GroceryList getGroceryList() {
+		
+	}
+	
+	public GroceryList GetGroceryListById(long GroceryList) {
+		
+	}
+	
+	public Arraylist<GroceryList> getGroceryListByGroup(group Group) {
+		
+	}
+	
+	public ArrayList<Article> getArticles() {
+		
+	}
+	
+	public void setName(String name) {
+		
+	}
+}
