0e3bf9e44ec61e73cfe1d10221babcaa552c949f
Patrick <anyeong@hotmail.co.kr>
2019-05-13 10:35:34 +0200

    GroupForm and GorceryListForm added for later use

---
+++ src/de/hdm/itprojektss19/team03/scart/shared/bo/GroceryListForm.java
@@ -0,0 +1,12 @@
+package de.hdm.itprojektss19.team03.scart.shared.bo;
+
+public class GroceryListForm {
+
+	public void setSelectedGroceryList(GroceryList selectedGroceryList) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
