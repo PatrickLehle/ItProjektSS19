4488e2927407a4cdcbd4025c1f63e0eab8c6bb99
Patrick <anyeong@hotmail.co.kr>
2019-05-19 05:23:45 +0200

    changes in EditorService/Async, Scart, EditorServiceImpl and more...

--- src/de/hdm/itprojektss19/team03/scart/server/ReportGeneratorImpl.java
+++ src/de/hdm/itprojektss19/team03/scart/server/ReportGeneratorImpl.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.hdm.itprojektss19.team03.scart.server.EditorServiceImpl;
+import de.hdm.itprojektss19.team03.scart.shared.EditorService;
 import de.hdm.itprojektss19.team03.scart.shared.ReportGenerator;
 import de.hdm.itprojektss19.team03.scart.shared.bo.Article;
 import de.hdm.itprojektss19.team03.scart.shared.bo.Retailer;
@@ -11,9 +13,24 @@ import de.hdm.itprojektss19.team03.scart.shared.bo.Retailer;
 
 public class ReportGeneratorImpl extends RemoteServiceServlet implements
 ReportGenerator{
+	
+	private static final long serialVersionUID = 1L;
+	private EditorService editorService = null;
+	
+	/**
+	 * *************************************************************************
+	 * INITIALISIERUNG
+	 * *************************************************************************
+	 */
 
-	public ReportGeneratorImpl() {
+	public ReportGeneratorImpl() throws IllegalArgumentException{
 		
+		EditorServiceImpl impl = new EditorServiceImpl();
+		impl.init();
+		this.editorService = impl;
+	}
+	protected EditorService getEditorService() {
+		return this.editorService;
 	}
 	
 	public void init() {
@@ -21,18 +38,26 @@ ReportGenerator{
 	}
 	
 	public ArrayList<Article> getStatistic(int UID) {
-		
+		if (this.getEditorService() == null) {
+			return null;
+		}
 	}
 	
 	public ArrayList<Article> getStatistic(int UID, Date start, Date end) {
-		
+		if (this.getEditorService() == null) {
+			return null;
+		}
 	} 
 	
 	public ArrayList<Article> getStatistic(int UID, Retailer retailer) {
-		
+		if (this.getEditorService() == null) {
+			return null;
+		}
 	}
 	
 	public ArrayList<Article> getStatistic(int UID, Date start, Date end, Retailer retailer) {
-		
+		if (this.getEditorService() == null) {
+			return null;
+		}
 	} 
 }
