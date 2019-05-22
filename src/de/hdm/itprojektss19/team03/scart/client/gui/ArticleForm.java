42d02564c771d13e82e60337299ecea1ec745fb4
Tom <th116@hdm-stuttgart.de>
2019-05-22 16:47:15 +0200

    minor change ArticleForm

--- src/de/hdm/itprojektss19/team03/scart/client/gui/ArticleForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/ArticleForm.java
@@ -1,19 +1,22 @@
 package de.hdm.itprojektss19.team03.scart.client.gui;
 
-import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ListBox;
 
 import de.hdm.itprojektss19.team03.scart.shared.bo.Article;
 
 public class ArticleForm extends VerticalPanel {
 
 	TextBox articleTextBox = new TextBox();
+	TextBox quantityTextBox = new TextBox();
+	//ListBox unitListBox = new ListBox();
 	Label newArticle = new Label("Artikel: ");
 	Button cancelButton = new Button("Abbrechen");
 	Button addButton = new Button("Artikel Hinzufügen");
+	
 	
 	
 	public ArticleForm(Article article) {
