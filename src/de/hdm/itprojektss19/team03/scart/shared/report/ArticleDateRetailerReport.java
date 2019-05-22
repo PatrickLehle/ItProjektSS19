191bf8c1b61f015a0a3eaa42036cc15c2d7f315f
Patrick <anyeong@hotmail.co.kr>
2019-05-22 16:41:53 +0200

    ArticleReport classes added

---
+++ src/de/hdm/itprojektss19/team03/scart/shared/report/ArticleDateRetailerReport.java
@@ -0,0 +1,13 @@
+package de.hdm.itprojektss19.team03.scart.shared.report;
+
+
+/**
+ * Reportklasse die eine Abfrage ueber alle Article in einem gewissen Zeitraum mit dem Retailer ausgibt.
+ * @author PatrickLehle
+ *
+ */
+public class ArticleDateRetailerReport extends SimpleReport{
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/de/hdm/itprojektss19/team03/scart/shared/report/ArticleReport.java
@@ -0,0 +1,11 @@
+package de.hdm.itprojektss19.team03.scart.shared.report;
+
+/**
+ * Reportklasse die eine Abfrage ueber alle Article ausgibt.
+ * @author PatrickLehle
+ *
+ */
+public class ArticleReport extends SimpleReport {
+
+	private static final long serialVersionUID = 1L;
+}
