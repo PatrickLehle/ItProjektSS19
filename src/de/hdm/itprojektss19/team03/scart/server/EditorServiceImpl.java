a5f6ac2a7e69598a8533bb73d387d312279161cc
Marco Dell'Oso <marco.delloso@gmail.com>
2019-05-22 17:12:21 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/de/hdm/itprojektss19/team03/scart/server/EditorServiceImpl.java
+++ src/de/hdm/itprojektss19/team03/scart/server/EditorServiceImpl.java
@@ -20,28 +20,25 @@ import de.hdm.itprojektss19.team03.scart.server.db.GroupMapper;
 import de.hdm.itprojektss19.team03.scart.server.db.GroceryListMapper;
 import de.hdm.itprojektss19.team03.scart.server.db.ArticleMapper;
 import de.hdm.itprojektss19.team03.scart.server.db.UnitMapper;
-import de.hdm.itprojektss19.team03.scart.server.db.EntryMapper;
 import de.hdm.itprojektss19.team03.scart.server.db.RetailerMapper;
 import de.hdm.itprojektss19.team03.scart.*;
 
 /**
  * 
- * @author bastiantilk, PatrickLehle
- * Serverseitiger RPC-Service fuer den Editor.
+ * @author bastiantilk, PatrickLehle Serverseitiger RPC-Service fuer den Editor.
  * 
  */
-public class EditorServiceImpl extends RemoteServiceServlet implements
-EditorService{
-	
+public class EditorServiceImpl extends RemoteServiceServlet implements EditorService {
+
 	public EditorServiceImpl() throws IllegalArgumentException {
-		
+
 	}
-	
+
 	/**
 	 * Serialisierung
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Die Mapperklasse wird referenziert, die die <code>User</code>-mit der
 	 * Datenbank vergleicht.
@@ -52,190 +49,184 @@ EditorService{
 	 * Die Mapperklasse wird referenziert, die die <code>Group</code>-mit der
 	 * Datenbank vergleicht.
 	 */
-	private gMapper gMapper = null;
+	private GroupMapper gMapper = null;
 
 	/**
-	 * Die Mapperklasse wird referenziert, die die <code>Retailer</code>-mit
-	 * der Datenbank vergleicht.
+	 * Die Mapperklasse wird referenziert, die die <code>Retailer</code>-mit der
+	 * Datenbank vergleicht.
 	 */
 	private RetailerMapper rMapper = null;
 
 	/**
-	 * Die Mapperklasse wird referenziert, die die
-	 * <code>ArticleMapper</code> mit der Datenbank vergleicht.
+	 * Die Mapperklasse wird referenziert, die die <code>ArticleMapper</code> mit
+	 * der Datenbank vergleicht.
 	 */
 	private ArticleMapper aMapper = null;
 
 	/**
-	 * Die Mapperklasse wird referenziert, die die <code>Unit</code> mit
-	 * der Datenbank vergleicht.
+	 * Die Mapperklasse wird referenziert, die die <code>Unit</code> mit der
+	 * Datenbank vergleicht.
 	 */
 	private UnitMapper unMapper = null;
 
 	/**
-	 * Die Mapperklasse wird referenziert, die die <code>Entry</code> mit
-	 * der Datenbank vergleicht.
+	 * Die Mapperklasse wird referenziert, die die <code>Entry</code> mit der
+	 * Datenbank vergleicht.
 	 */
-	private EntryMapper eMapper = null;
+//	private EntryMapper eMapper = null;
 
 	/**
-	 * Die Mapperklasse wird referenziert, die die <code>GroceryList</code> mit
-	 * der Datenbank vergleicht.
+	 * Die Mapperklasse wird referenziert, die die <code>GroceryList</code> mit der
+	 * Datenbank vergleicht.
 	 */
 	private GroceryListMapper glMapper = null;
-	
-	
+
 	/**
 	 * *****************************************************************************
 	 * **** Initialisierung
 	 * *****************************************************************************
 	 * ****
 	 */
-	
+
 	public void init() throws IllegalArgumentException {
-		
+
 		this.uMapper = UserMapper.userMapper();
 		this.gMapper = GroupMapper.groupMapper();
 		this.glMapper = GroceryListMapper.groceryListMapper();
 		this.aMapper = ArticleMapper.articleMapper();
-		this.eMapper = EntryMapper.entryMapper();
+//		this.eMapper = EntryMapper.entryMapper();
 		this.rMapper = RetailerMapper.retailerMapper();
 		this.unMapper = UnitMapper.unitMapper();
-		
+
 	}
-	public User createUser(String username, String emailAdress) throws IllegalArgumentException{
-		//E-Mail und Username muss zunaechst ueber GUI abgefragt werden
-		
-		
+
+	public User createUser(String username, String emailAdress) throws IllegalArgumentException {
+		// E-Mail und Username muss zunaechst ueber GUI abgefragt werden
+
 		try {
-			
-			Vector<User> results= uMapper.findByUserEmail(emailAdress);
-			
-			if(results.size() > 1) {
-				
-				//Email bereits vorhanden
+
+			User result = uMapper.findByUserEmail(emailAdress);
+
+			if (result != null) {
+				return result;
+				// Email bereits vorhanden
 			} else {
-				User temp = new User(username, emailAdress);
-				uMapper.insert(temp);
-				//Ausgabe der Rueckgabe aus der insert Funktion fehlt
+				User newUser = new User(username, emailAdress);
+				uMapper.insert(newUser);
+				return newUser;
+				// Ausgabe der Rueckgabe aus der insert Funktion fehlt
 			}
-		
-		} catch(IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-	}
-	
 
-	public void deleteUser(User u) throws IllegalArgumentException{
-		//Sollte es ueberhaupt erlaubt sein einen User zu loeschen?
-		
-		try { 
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public void deleteUser(User u) throws IllegalArgumentException {
+		// Sollte es ueberhaupt erlaubt sein einen User zu loeschen?
+
+		try {
 			uMapper.delete(u);
-			
-		} catch(IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	public User getUserById(int userId) throws IllegalArgumentException{
-		try { 
-			Vector<User> foundUsers = uMapper.findbyUserId(userId);
-			
-			//Fehlt noch: Ausgabe des foundUsers Vektor
-			
-			
-		} catch(IllegalArgumentException e) {
-			e.printStackTrace();
-		}
-	}
-	
 
-	public User getUserByGMail(String email) throws IllegalArgumentException{
-		try { 
-			Vector<User> foundUsers = uMapper.findByUserEmail(email);
-			
-			//Fehlt noch: Ausgabe des 'foundUsers' Vektor
-			
-			
-		} catch(IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
+	public User getUserById(int userId) throws IllegalArgumentException {
+		try {
+			User foundUser = uMapper.findbyUserId(userId);
+			return foundUser;
+
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public User getUserByGMail(String email) throws IllegalArgumentException {
+		try {
+			User foundUser = uMapper.findByUserEmail(email);
+			return foundUser;
+
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	/**
-	 * **********************************************************************
-	 * Group
+	 * ********************************************************************** Group
 	 * **********************************************************************
 	 */
-	
-	public Group createGroup(Group g) throws IllegalArgumentException{
-		
-	}
-	
-	public void saveGroup(Group g) throws IllegalArgumentException{
-		
-	}
-	
-	public void deleteGroup(Group g) throws IllegalArgumentException{
-		
-	}
-	
-	public Group getGrouById(int groupId) throws IllegalArgumentException{
-		
-	}
-	
-	public Vector<Group> getAllGroupsByUser(User u) throws IllegalArgumentException{
-		
+
+	public Group createGroup(Group g) throws IllegalArgumentException {
+		return GroupMapper.groupMapper().insert(g);
 	}
 
-	public void addUserToGroup(User u, Group g) throws IllegalArgumentException{
-		
+	public void saveGroup(Group g) throws IllegalArgumentException {
+		GroupMapper.groupMapper().update(g);
 	}
-	
-	public void leaveGroup(User u, Group g) throws IllegalArgumentException{
-		
+
+	public void deleteGroup(Group g) throws IllegalArgumentException {
+		GroupMapper.groupMapper().delete(g);
 	}
-	
+
+	public Group getGrouById(int groupId) throws IllegalArgumentException {
+		return GroupMapper.groupMapper().findbyGroupId(groupId);
+
+	}
+
+	public Vector<Group> getAllGroupsByUser(User u) throws IllegalArgumentException {
+		return GroupMapper.groupMapper().findAll();
+	}
+
+	public void addUserToGroup(User u, Group g) throws IllegalArgumentException {
+		g.addUser(u);
+		GroupMapper.groupMapper().update(g);
+	}
+
+	public void leaveGroup(User u, Group g) throws IllegalArgumentException {
+
+	}
+
 	public Vector<Group> statusSharingGroup(Vector<Group> result) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
 	/**
 	 * **********************************************************************
 	 * GroceryList
 	 * **********************************************************************
 	 */
-	
-	public GroceryList createGroceryList(String name, GroceryList gl) throws IllegalArgumentException{
-		
+
+	public GroceryList createGroceryList(String name, GroceryList gl) throws IllegalArgumentException {
+
 	}
-	
-	public void saveGroceryList(GroceryList gl) throws IllegalArgumentException{
-		
+
+	public void saveGroceryList(GroceryList gl) throws IllegalArgumentException {
+
 	}
-	
-	public void deleteGroceryList(GroceryList gl) throws IllegalArgumentException{
-		
+
+	public void deleteGroceryList(GroceryList gl) throws IllegalArgumentException {
+
 	}
-	
+
 	public Vector<GroceryList> getGroceryListByOwner(User u) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	//public Vector<GroceryList> statusSharingGroceryList(Vector<GroceryList> result, AsyncCallback<Vector<GroceryList>> asyncCallback);
+	// public Vector<GroceryList> statusSharingGroceryList(Vector<GroceryList>
+	// result, AsyncCallback<Vector<GroceryList>> asyncCallback);
 
-	public GroceryList getGroceryListById(Object groceryListId) throws IllegalArgumentException{
-		
+	public GroceryList getGroceryListById(Object groceryListId) throws IllegalArgumentException {
+
 	}
-	
+
 	public Vector<GroceryList> statusSharingGroceryList(Vector<GroceryList> result) {
 		// TODO Auto-generated method stub
 		return null;
@@ -245,86 +236,84 @@ EditorService{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	/**
 	 * **********************************************************************
 	 * Article
 	 * **********************************************************************
 	 */
-	
-	public Article createArticle(Article a) throws IllegalArgumentException{
-		//Input fuer Article Attribute muss noch erledigt werden
-		
-		
+
+	public Article createArticle(Article a) throws IllegalArgumentException {
+		// Input fuer Article Attribute muss noch erledigt werden
+
 		try {
-			Article temp = new Article(); //Mit dem Input der Attribute muss hier ein neues Object mit den Attributen erstellt werden
+			Article temp = new Article(); // Mit dem Input der Attribute muss hier ein neues Object mit den Attributen
+											// erstellt werden
 			aMapper.insert(temp);
-			//Ausgabe der Rueckgabe aus der insert Funktion fehlt
-					
-		
+			// Ausgabe der Rueckgabe aus der insert Funktion fehlt
+
 		} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-		}
-	}
-	
-	public void saveArticle(Article a) throws IllegalArgumentException{
-		
-	}
-	
-	public void deleteArticle(Article a) throws IllegalArgumentException{
-		//Sollten Articles ueberhaupt geloescht werden
-		
-		try { 
-			aMapper.delete(a);
-			//Erfolgts Message fuer erfolgreiches Loeschen
-			
-		} catch(IllegalArgumentException e) {
 			e.printStackTrace();
 		}
-		
 	}
-	
-	public Article getArticleById(int articleId) throws IllegalArgumentException{
-		aMapper.findByKey(articleId); //Ausgabe fuer diese Article-Objekt muss noch hinzugefuegt werden
+
+	public void saveArticle(Article a) throws IllegalArgumentException {
+
 	}
-	
+
+	public void deleteArticle(Article a) throws IllegalArgumentException {
+		// Sollten Articles ueberhaupt geloescht werden
+
+		try {
+			aMapper.delete(a);
+			// Erfolgts Message fuer erfolgreiches Loeschen
+
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public Article getArticleById(int articleId) throws IllegalArgumentException {
+		aMapper.findByKey(articleId); // Ausgabe fuer diese Article-Objekt muss noch hinzugefuegt werden
+	}
+
 	/**
 	 * **********************************************************************
 	 * Retailer
 	 * **********************************************************************
 	 */
-	
-	public Retailer createRetailer(Retailer r) throws IllegalArgumentException{
-		
+
+	public Retailer createRetailer(Retailer r) throws IllegalArgumentException {
+
 	}
-	
-	public void saveRetailer(Retailer r) throws IllegalArgumentException{
-		
+
+	public void saveRetailer(Retailer r) throws IllegalArgumentException {
+
 	}
-	
-	public void deleteRetailer(Retailer r) throws IllegalArgumentException{
-		
+
+	public void deleteRetailer(Retailer r) throws IllegalArgumentException {
+
 	}
-	
-	public Article getArticleByRetailer(Retailer r) throws IllegalArgumentException{
-		
+
+	public Article getArticleByRetailer(Retailer r) throws IllegalArgumentException {
+
 	}
-	
-	public Vector<Article> getArticleByDate(Date start, Date end) throws IllegalArgumentException{
-		
+
+	public Vector<Article> getArticleByDate(Date start, Date end) throws IllegalArgumentException {
+
 	}
-	
-	public Vector<Article> getArticleByDateRetailer(Date start, Date end, Retailer r) throws IllegalArgumentException{
-		
+
+	public Vector<Article> getArticleByDateRetailer(Date start, Date end, Retailer r) throws IllegalArgumentException {
+
 	}
-	
-	public Retailer getRetailerById(int retailerId) throws IllegalArgumentException{
-		
+
+	public Retailer getRetailerById(int retailerId) throws IllegalArgumentException {
+
 	}
-	
+
 	/**
-	 * **********************************************************************
-	 * Unit
+	 * ********************************************************************** Unit
 	 * **********************************************************************
 	 */
 	
@@ -351,42 +340,40 @@ EditorService{
 	public void saveUnit(Unit u) throws IllegalArgumentException{
 		
 	}
-	
-	public void deleteUnit(Unit u) throws IllegalArgumentException{
-		
+
+	public void deleteUnit(Unit u) throws IllegalArgumentException {
+
 	}
-	
-	public Unit getUnitById(int unitId) throws IllegalArgumentException{
-		
+
+	public Unit getUnitById(int unitId) throws IllegalArgumentException {
+
 	}
-	
-	public Unit getUnitByName(String unitName) throws IllegalArgumentException{
-		
+
+	public Unit getUnitByName(String unitName) throws IllegalArgumentException {
+
 	}
-	
+
 	/**
-	 * **********************************************************************
-	 * Entry
+	 * ********************************************************************** Entry
 	 * **********************************************************************
 	 */
-	
-	public Entry createEntry(Entry e) throws IllegalArgumentException{
-		
+
+	public Entry createEntry(Entry e) throws IllegalArgumentException {
+
 	}
-	
-	public void saveEntry(Entry e) throws IllegalArgumentException{
-		
+
+	public void saveEntry(Entry e) throws IllegalArgumentException {
+
 	}
-	
-	public void deleteEntry(Entry e) throws IllegalArgumentException{
-		
+
+	public void deleteEntry(Entry e) throws IllegalArgumentException {
+
 	}
+
 	@Override
 	public User createUser(String emailAdress) throws IllegalArgumentException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
 }
-
