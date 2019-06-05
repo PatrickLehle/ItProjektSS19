c7cd3b28f2dc7c90adf91ba4bc0d270cd1adbec3
Tom <th116@hdm-stuttgart.de>
2019-06-05 16:40:33 +0200

    Changes to ArticleForm and GlForm

--- src/de/hdm/itprojektss19/team03/scart/client/gui/ArticleForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/ArticleForm.java
@@ -3,21 +3,67 @@ package de.hdm.itprojektss19.team03.scart.client.gui;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
-//import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 
+import de.hdm.itprojektss19.team03.scart.client.ClientsideSettings;
+import de.hdm.itprojektss19.team03.scart.shared.EditorServiceAsync;
 import de.hdm.itprojektss19.team03.scart.shared.bo.Article;
 
 public class ArticleForm extends VerticalPanel {
 
+	private EditorServiceAsync ev = ClientsideSettings.getEditorVerwaltung();
+
+	VerticalPanel vt = new VerticalPanel();
+	HorizontalPanel hp = new HorizontalPanel();
 	TextBox articleTextBox = new TextBox();
 	TextBox quantityTextBox = new TextBox();
-	//ListBox unitListBox = new ListBox();
 	Label newArticle = new Label("Artikel: ");
-	Button cancelButton = new Button("Abbrechen");
-	Button addButton = new Button("Artikel Hinzufügen");
+	Button cancelBtn = new Button("Abbrechen");
+	Button addBtn = new Button("Hinzufügen");
+	Article articleToDisplay = null;
+
 	
-	
+	public void onLoad() {
+		super.onLoad();
+		
+		vt.add(addBtn);
+		addBtn.addClickHandler(new AddClickHandler());
+		addBtn.setEnabled(true);
+		
+		vt.add(cancelBtn);
+		cancelBtn.addClickHandler(new DeleteClickHandler());
+		cancelBtn.setEnabled(true);
+		
+		this.add(hp);
+		
+	}
+		
+	private class DeleteClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent e) {
+			if (articleToDisplay == null) {
+				
+			} else {
+			}
+		}
+	}
+	private class AddClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent e) {
+			if (articleToDisplay != null) {	
+				
+			} else {
+				Window.alert("Bitte Artikel eintragen.");
+			}
+		}
+	}	
+
 	
 	public ArticleForm(Article article) {
 		// TODO Auto-generated constructor stub
--- src/de/hdm/itprojektss19/team03/scart/client/gui/GroceryListForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/GroceryListForm.java
@@ -2,21 +2,26 @@ package de.hdm.itprojektss19.team03.scart.client.gui;
 
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-//import com.google.gwt.user.cellview.client.CellTree;
 import com.google.gwt.user.client.ui.Button;
-//import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
+import de.hdm.itprojektss19.team03.scart.client.ClientsideSettings;
+import de.hdm.itprojektss19.team03.scart.shared.EditorServiceAsync;
 import de.hdm.itprojektss19.team03.scart.shared.bo.Article;
 
 public class GroceryListForm extends VerticalPanel {
+	
+
+	private EditorServiceAsync ev = ClientsideSettings.getEditorVerwaltung();
+	
 
 	HorizontalPanel hp = new HorizontalPanel();
 	Button newArticleBtn = new Button(
 			"<image src='/images/plusButton.png' width='16px' height='16px' align='center'/>");
+	
 
 	public GroceryListForm(final Article article) {
 
@@ -32,23 +37,40 @@ public class GroceryListForm extends VerticalPanel {
 				RootPanel.get("content").add(new ArticleForm(article));
 			}
 		});
-		
+
 		newArticleBtn.setPixelSize(100, 60);
 		newArticleBtn.setStyleName("button1");
 		newArticleBtn.setTitle("add Article");
-		
-		hp.add(newArticleBtn);
-		this.add(hp);
-		this.add(sc);
+
+
 	}
 
+	
+//	void setArticle(Article a) {
+//			if (a == null) {
+//				cancelBtn.setEnabled(true);
+//				addBtn.setEnabled(true);
+//				firstNameTextBox.setText(customerToDisplay.getFirstName());
+//				lastNameTextBox.setText(customerToDisplay.getLastName());
+//				idValueLabel.setText("Kunde: " + Integer.toString(customerToDisplay.getId()));
+//			} else {
+//				firstNameTextBox.setText("");
+//				lastNameTextBox.setText("");
+//				idValueLabel.setText("Kunde: ");
+//				deleteButton.setEnabled(false);
+//				newButton.setEnabled(false);
+//		}
+//		}
+		
 	public GroceryListForm(de.hdm.itprojektss19.team03.scart.client.gui.GroceryListForm groceryList) {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void setSelectedGroceryListForm(GroceryListForm selectedGroceryList) {
-		// TODO Auto-generated method stub
-		
-	}
+			public static void setSelectedGroceryListForm(GroceryListForm selectedGroceryList) {
+				// TODO Auto-generated method stub
 
+			}
+
+		
+	
 }
