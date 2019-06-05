43419b7b37f6f690a714f84f966533faf3eaa418
Patrick <anyeong@hotmail.co.kr>
2019-06-05 15:05:22 +0200

    dbconnection error fixed

--- src/de/hdm/itprojektss19/team03/scart/server/db/DBConnection.java
+++ src/de/hdm/itprojektss19/team03/scart/server/db/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
 
 	private static Connection con = null;
 	private static String googleUrl = "jdbc:google:mysql://itpss19scart:sontactinstanz/scartdb";
-	private static String localUrl = "jdbc:mysql://localhost:8889/ITPROJEKTSS19?user=patrick&password=patrick";
+	private static String localUrl = "jdbc:mysql://localhost:8000/ITPROJEKTSS19?user=patrick&password=patrick";
 
 
 	private static final String username = "patrick";
