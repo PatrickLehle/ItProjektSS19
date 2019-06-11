0f7df8f6f7185e2e55035f1253ddea0023822fb8
Van Duy Ho <v.duy.ho@gmail.com>
2019-06-11 14:20:47 +0200

    Update labels, buttons, panels

--- src/de/hdm/itprojektss19/team03/scart/client/gui/ProfileForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/ProfileForm.java
@@ -16,9 +16,10 @@ public class ProfileForm extends VerticalPanel {
 	
 	
 	//LABELS================================================
-	Label newProfile = new Label("Dein Profil: ");
+	Label newProfile = new Label("Dein Profil");
 	Label firstName = new Label("Vorname: ");
-	Label lastName = new Label("Nachname :");
+	Label lastName = new Label("Nachname: ");
+	Label emailAdress = new Label("E-Mail: ");
 
 	
 	//BUTTONS===============================================
@@ -27,7 +28,11 @@ public class ProfileForm extends VerticalPanel {
 	
 	
 	//PANELS================================================
+	
 	HorizontalPanel newProfilePanel = new HorizontalPanel(); //Panel fuer Ueberschrift
+	HorizontalPanel firstNamePanel = new HorizontalPanel(); //Panel fuer Vorname
+	HorizontalPanel lastNamePanel = new HorizontalPanel(); //Panel fuer Nachname
+	HorizontalPanel emailAdressPanel = new HorizontalPanel(); //Panel fuer E-Mail Adresse
 	HorizontalPanel buttonPanel = new HorizontalPanel(); // Panel fuer Buttons
 	
 	/**
