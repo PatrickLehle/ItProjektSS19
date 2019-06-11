72a4f8439b53bd8833956a1efb881bb8e2eef3fa
Van Duy Ho <v.duy.ho@gmail.com>
2019-06-11 12:31:49 +0200

    Rename class ProfileForm

---
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/ProfileForm.java
@@ -0,0 +1,11 @@
+package de.hdm.itprojektss19.team03.scart.client.gui;
+
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import com.google.gwt.user.client.ui.HorizontalPanel;
+
+public class ProfileForm {
+	
+	
+
+}
