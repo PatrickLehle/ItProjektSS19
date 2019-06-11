97065c8d5152da9abed5a9a8e6636289a0159189
Unknown <jh187@hdm-stuttgart.de>
2019-06-11 15:21:06 +0200

    first edits in GroupForm

--- src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
@@ -1,11 +1,27 @@
 package de.hdm.itprojektss19.team03.scart.client.gui;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
 /**
  * 
  * @author Julian Hofer
  *
  */
 
-public class GroupForm {
+public class GroupForm extends VerticalPanel {
+	
+	
+	Label GroupLabel = null;
+	HorizontalPanel GroupNamePanel = null;
+	Label GroupNameLabel = null;
+	
+	Button GroupInfoButton = null;
+	
+	
+	
+	
 
 }
