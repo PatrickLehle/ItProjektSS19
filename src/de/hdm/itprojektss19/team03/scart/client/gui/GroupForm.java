51cb5d8490476359c7901e407ca02fd5a98a9a14
Unknown <jh187@hdm-stuttgart.de>
2019-06-12 18:41:51 +0200

    added Panels, Labels, Buttons to GroupForm

--- src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
@@ -3,7 +3,13 @@ package de.hdm.itprojektss19.team03.scart.client.gui;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojektss19.team03.scart.shared.bo.User;
+import de.hdm.itprojektss19.team03.scart.client.ClientsideSettings;
+import de.hdm.itprojektss19.team03.scart.shared.EditorServiceAsync;
+import de.hdm.itprojektss19.team03.scart.shared.bo.Group;
 
 /**
  * 
@@ -13,14 +19,45 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class GroupForm extends VerticalPanel {
 	
+	private EditorServiceAsync ev = ClientsideSettings.getEditorVerwaltung();
 	
-	Label GroupLabel = null;
+	// PANELS
+	VerticalPanel GroupFormPanel = new VerticalPanel();
 	HorizontalPanel GroupNamePanel = null;
+	
+	// Labels
+	Label GroupLabel = new Label("Gruppenname");
 	Label GroupNameLabel = null;
 	
-	Button GroupInfoButton = null;
+	//Buttons
+	Button GroupInfoButton = new Button("Gruppen verwalten");
+	Button createGroupButton = new Button("Gruppe hinzufügen");
 	
+	/**
+	 * default Konstruktor
+	 */
+	public GroupForm() {
+		
+	}
 	
+	public GroupForm(User user) {
+		
+		GroupNamePanel.setHorizontalAlignment(ALIGN_CENTER);
+		// GroupNamePanel.setStyleName(CSS); TO DO
+		GroupNameLabel.setHorizontalAlignment(ALIGN_CENTER);
+		GroupLabel.setHorizontalAlignment(ALIGN_CENTER);
+		
+		GroupFormPanel.add(GroupLabel);
+		GroupNamePanel.add(GroupNameLabel);
+		GroupFormPanel.add(GroupNamePanel);
+		
+		RootPanel.get("content").clear();
+		RootPanel.get("content").add(GroupFormPanel);
+		
+		
+		
+		
+	}
 	
 	
 
