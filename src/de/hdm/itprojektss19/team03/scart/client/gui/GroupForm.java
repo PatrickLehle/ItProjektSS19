023fb719c7b99dd53a07e6aee28cb6ba674533c2
Unknown <jh187@hdm-stuttgart.de>
2019-06-12 21:32:02 +0200

    added clickhandler, started with editing onload

--- src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
+++ src/de/hdm/itprojektss19/team03/scart/client/gui/GroupForm.java
@@ -1,5 +1,7 @@
 package de.hdm.itprojektss19.team03.scart.client.gui;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -10,16 +12,20 @@ import de.hdm.itprojektss19.team03.scart.shared.bo.User;
 import de.hdm.itprojektss19.team03.scart.client.ClientsideSettings;
 import de.hdm.itprojektss19.team03.scart.shared.EditorServiceAsync;
 import de.hdm.itprojektss19.team03.scart.shared.bo.Group;
+import de.hdm.itprojektss19.team03.scart.client.gui.EditGroup;
 
 /**
  * 
  * @author Julian Hofer
  *
+ *	ToDo CSS Style Panels, Labels, Buttons
  */
 
 public class GroupForm extends VerticalPanel {
 	
 	private EditorServiceAsync ev = ClientsideSettings.getEditorVerwaltung();
+	
+	Group group = null;
 	
 	// PANELS
 	VerticalPanel GroupFormPanel = new VerticalPanel();
@@ -31,7 +37,7 @@ public class GroupForm extends VerticalPanel {
 	
 	//Buttons
 	Button GroupInfoButton = new Button("Gruppen verwalten");
-	Button createGroupButton = new Button("Gruppe hinzufügen");
+	Button CreateGroupButton = new Button("Gruppe hinzufügen");
 	
 	/**
 	 * default Konstruktor
@@ -40,7 +46,9 @@ public class GroupForm extends VerticalPanel {
 		
 	}
 	
-	public GroupForm(User user) {
+	public GroupForm(Group g) {
+		
+		this.group = g;
 		
 		GroupNamePanel.setHorizontalAlignment(ALIGN_CENTER);
 		// GroupNamePanel.setStyleName(CSS); TO DO
@@ -51,14 +59,61 @@ public class GroupForm extends VerticalPanel {
 		GroupNamePanel.add(GroupNameLabel);
 		GroupFormPanel.add(GroupNamePanel);
 		
-		RootPanel.get("content").clear();
-		RootPanel.get("content").add(GroupFormPanel);
+		GroupFormPanel.add(GroupInfoButton);
+		GroupInfoButton.addClickHandler(new InfoClickHandler());
+		GroupInfoButton.setEnabled(true);
 		
+		GroupFormPanel.add(CreateGroupButton);
+		CreateGroupButton.addClickHandler(new CreateClickHandler());
+		CreateGroupButton.setEnabled(true);
 		
 		
 		
 	}
 	
+	public void onLoad() {
+		super.onLoad();
+		
+		RootPanel.get("contentHeader").clear();
+		RootPanel.get("content").clear();
+		RootPanel.get("footer").clear();
+		RootPanel.get("content").add(GroupFormPanel);
+		
+		for (int i = 0; i < group.getId(); i++ ) {
+			String g = group.getGroupName(null);
+			GroupNameLabel.add(g);
+			
+		}
+		
+		
+	}
 	
+	
+	class InfoClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent arg0) {
+			// TODO Auto-generated method stub
+			
+			RootPanel.get("content").clear();
+			//RootPanel.get("Navigator").add(EditGroup); 
+			
+			
+		}
+		
+		
+	}
+	
+	class CreateClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent arg0) {
+			// TODO Auto-generated method stub
+			
+			RootPanel.get("content").clear();
+			RootPanel.get("createGroup");
+		}
+		
+	}
 
 }
