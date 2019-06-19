16522ecd610530ce4ca79b6eaeda5fd82ae463b3
Marco Dell'Oso <marco.delloso@gmail.com>
2019-06-19 14:44:32 +0200

    add login

--- src/de/hdm/itprojektss19/team03/scart/client/Scart.java
+++ src/de/hdm/itprojektss19/team03/scart/client/Scart.java
@@ -1,8 +1,11 @@
 package de.hdm.itprojektss19.team03.scart.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -10,12 +13,16 @@ import com.google.gwt.user.client.ui.RootPanel;
 import de.hdm.itprojektss19.team03.scart.client.gui.FooterForm;
 import de.hdm.itprojektss19.team03.scart.client.gui.GroceryListForm;
 import de.hdm.itprojektss19.team03.scart.client.gui.GroupForm;
+import de.hdm.itprojektss19.team03.scart.client.gui.LoginForm;
 import de.hdm.itprojektss19.team03.scart.client.gui.ProfilForm;
 import de.hdm.itprojektss19.team03.scart.client.gui.ToolbarForm;
+import de.hdm.itprojektss19.team03.scart.shared.LoginService;
+import de.hdm.itprojektss19.team03.scart.shared.LoginServiceAsync;
+import de.hdm.itprojektss19.team03.scart.shared.bo.LoginInfo;
 
 /**
- * Die Klasse EntryPoint beinhaltet die Methode <code>onModuleLoad()</code> die
- * beim Start der Applikation als erstes aufgerufen wird.
+ * Die Scart classe implementiert EntryPoint, d.h. sie wird als erstes
+ * aufgerufen, sobald die Anwendung gestartet wird.
  * 
  * @author PatrickLehle
  * @author Marco Dell'Oso
@@ -23,22 +30,76 @@ import de.hdm.itprojektss19.team03.scart.client.gui.ToolbarForm;
  */
 public class Scart implements EntryPoint {
 
+	private LoginServiceAsync loginService = GWT.create(LoginService.class);
+
 	private ToolbarForm toolbar = new ToolbarForm();
 	private FooterForm footer = new FooterForm();
-	private Button button1 = new Button("grocery list");
-	private Button button2 = new Button("profile ");
-	private HorizontalPanel contentPanel = new HorizontalPanel();
-
 	private GroceryListForm groceryListForm = new GroceryListForm();
 	private GroupForm groupForm = new GroupForm();
 	private ProfilForm profilForm = new ProfilForm();
 
+	private HorizontalPanel contentPanel = new HorizontalPanel();
+
+	// @todo: delete test buttons
+	private Button button1 = new Button("grocery list");
+	private Button button2 = new Button("profile ");
+
+	/**
+	 * startet, sobald das Modul geladen wird.
+	 */
 	public void onModuleLoad() {
 		contentPanel.setSpacing(30);
 
+		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
+
+			public void onFailure(Throwable err) {
+				Window.alert(err.getMessage());
+			}
+
+			public void onSuccess(LoginInfo logInfo) {
+				/**
+				 * Check if the user is logged in
+				 */
+				if (logInfo.isLoggedIn()) {
+					loadPage();
+				} else {
+					login(logInfo.getLoginUrl());
+				}
+			}
+
+		});
+
+		/**
+		 * Add header and footer to the (root-)Panels they belong to
+		 */
+		RootPanel.get("header").clear();
+		RootPanel.get("header").add(toolbar);
+		RootPanel.get("footer").clear();
+		RootPanel.get("footer").add(footer);
+	}
+
+	/**
+	 * Load the login Form into content panel
+	 */
+	private void login(String logURL) {
+		LoginForm loginForm = new LoginForm(logURL);
+		RootPanel.get("content").clear();
+		RootPanel.get("content").add(loginForm);
+	}
+
+	/**
+	 * Add content to content panel
+	 */
+	private void loadPage() {
+		contentPanel.add(groupForm);
+		contentPanel.add(button1);
+		contentPanel.add(button2);
+		RootPanel.get("content").clear();
+		RootPanel.get("content").add(contentPanel);
+
+		// @todo: delete test buttons
 		button1.addClickHandler(new ClickHandler() {
 
-			@Override
 			public void onClick(ClickEvent evt) {
 				RootPanel.get("content").clear();
 				RootPanel.get("content").add(groceryListForm);
@@ -54,16 +115,6 @@ public class Scart implements EntryPoint {
 
 			}
 		});
-
-		/**
-		 * Add elemets to the (root-)Panels they belong to
-		 */
-		contentPanel.add(groupForm);
-		contentPanel.add(button1);
-		contentPanel.add(button2);
-		RootPanel.get("header").add(toolbar);
-		RootPanel.get("content").add(contentPanel);
-		RootPanel.get("footer").add(footer);
 	}
 
 }
